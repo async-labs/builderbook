47d797e84e335c4b28145ac5cd8301bf9fe17d1d
Tima <dudeness101@gmail.com>
2018-03-06 21:24:56 -0800

    remove listname

--- book/8-end/server/mailchimp.js
+++ book/8-end/server/mailchimp.js
@@ -8,16 +8,14 @@ export async function subscribe({ email }) {
     status: 'subscribed',
   };
 
-  const LIST_IDS = {
-    purchased: process.env.MAILCHIMP_PURCHASED_LIST_ID,
-  };
+  const LIST_IDS = process.env.MAILCHIMP_PURCHASED_LIST_ID;
 
   const API_KEY = process.env.MAILCHIMP_API_KEY;
 
   await new Promise((resolve, reject) => {
     request.post(
       {
-        uri: `https://us17.api.mailchimp.com/3.0/lists/${LIST_IDS[listName]}/members/`,
+        uri: `https://us17.api.mailchimp.com/3.0/lists/${LIST_IDS}/members/`,
         headers: {
           Accept: 'application/json',
           Authorization: `Basic ${Buffer.from(`apikey:${API_KEY}`).toString('base64')}`,
