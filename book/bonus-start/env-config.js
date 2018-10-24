c080aa5673379f8d66fe5325c5dabb4c6334a581
klyburke <kellyburke101@gmail.com>
2018-10-24 13:47:37 -0700

    remove bonus book code

--- book/bonus-start/env-config.js
+++
@@ -1,7 +0,0 @@
-const dev = process.env.NODE_ENV !== 'production';
-
-module.exports = {
-  StripePublishableKey: dev
-    ? process.env.Stripe_Test_PublishableKey
-    : process.env.Stripe_Live_PublishableKey,
-};
--- book/bonus-start/server/logs.js
+++
@@ -1,9 +0,0 @@
-import winston from 'winston';
-
-const logger = winston.createLogger({
-  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-  format: winston.format.simple(),
-  transports: [new winston.transports.Console()],
-});
-
-export default logger;
