dcd5f0073b84d5bea0bcc38579759360d719345d
tima101 <dudeness101@gmail.com>
2019-01-18 13:03:32 -0800

    #130: winston bug

--- book/6-start/server/logs.js
+++ book/6-start/server/logs.js
@@ -2,7 +2,7 @@ const winston = require('winston');
 
 const logger = winston.createLogger({
   level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-  format: winston.format.simple(),
+  format: winston.format.combine(winston.format.splat(), winston.format.simple()),
   transports: [new winston.transports.Console()],
 });
 
