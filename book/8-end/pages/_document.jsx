0e95b0d123bb9ecc1ce45665032cc45bcf26f802
klyburke <kellyburke101@gmail.com>
2022-04-14 16:55:39 -0700

    remove GA scripts in 8-end, 9-begin #469

--- book/8-end/pages/_document.jsx
+++ book/8-end/pages/_document.jsx
@@ -74,22 +74,6 @@ class MyDocument extends Document {
               }
             `}
           </style>
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.GA_MEASUREMENT_ID}', {
-                page_path: window.location.pathname,
-              });
-            `,
-            }}
-          />
         </Head>
         <body>
           <Main />
