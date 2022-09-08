c59b23e474ac7b656d20e77fe57d89e35ec9ca3b
Jose Del Real <delreal.j02@mymail.sxu.edu>
2022-09-07 21:22:56 -0700

    Chapter 1 of builder book was completed

---
+++ book/1-begin/pages/_app.jsx
@@ -0,0 +1,42 @@
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import App from 'next/app';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import Head from 'next/head';
+import { theme } from '../lib/theme';
+
+import Header from '../components/Header';
+
+const propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired, // eslint-disable-line
+};
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+
+    // console.log(pageProps);
+
+    return (
+      <CacheProvider value={createCache({ key: 'css' })}>
+        <ThemeProvider theme={theme}>
+          <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          </Head>
+          <CssBaseline />
+          <Header {...pageProps} />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
+    );
+  }
+}
+
+MyApp.propTypes = propTypes;
+
+export default MyApp;
