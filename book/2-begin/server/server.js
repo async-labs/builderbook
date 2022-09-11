7aae62380b82a470546817396298d3222ae9323d
Nicole Salow <salow.n01@mymail.sxu.edu>
2022-09-11 16:52:41 -0500

    Chapter 2
    Sections done:
    -HTTP
    -Express Server

---
+++ book/2-begin/server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const next = require('next');
+
+const port = process.env.PORT || 8000;
+const ROOT_URL = `http://localhost:${port}`;
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get('/', (req, res) => {
+    const user = { email: 'team@builderbook.org' };
+    app.render(req, res, '/', { user });
+  });
+
+  server.get('*', (req, res) => handle(req, res));
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on ${ROOT_URL}`);
+  });
+});
