5b9078b1096628d5643fe2d4e677b8bdce2ab834
Dan Lam <lamphere.d01@mymail.sxu.edu>
2022-09-12 01:03:48 -0500

    chapter 2 work, does not function fully

---
+++ book/2-begin/components/MenuWithAvatar.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Avatar from '@mui/material/Avatar';
+
+const propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+  options: PropTypes.arrayOf(Object).isRequired,
+};
+
+class MenuWithAvatar extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      anchorEl: undefined,
+    };
+  }
+
+  handleClick = (event) => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  render() {
+    const { options, src, alt } = this.props;
+    const { anchorEl } = this.state;
+
+    return (
+      <div>
+        <Avatar
+          aria-controls={anchorEl ? 'simple-menu' : null}
+          aria-haspopup="true"
+          onClick={this.handleClick}
+          onKeyPress={this.handleClick}
+          src={src}
+          alt={alt}
+          style={{ margin: '0px 20px 0px auto', cursor: 'pointer' }}
+        />
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={this.handleClose}
+          keepMounted
+        >
+          {options.map((option) => (
+            <div id="wrappingLink" key={option.text}>
+              {option.anchor ? (
+                <MenuItem
+                  onClick={(event) => {
+                    event.preventDefault();
+                    window.location.href = option.href;
+                    this.handleClose();
+                  }}
+                >
+                  {option.text}
+                </MenuItem>
+              ) : (
+                <Link href={option.href} as={option.as || option.href}>
+                  <MenuItem>{option.text}</MenuItem>
+                </Link>
+              )}
+            </div>
+          ))}
+        </Menu>
+      </div>
+    );
+  }
+}
+
+MenuWithAvatar.propTypes = propTypes;
+
+export default MenuWithAvatar;
---
+++ book/2-begin/server/models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const mongoSchema = new Schema({
+  googleId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  googleToken: {
+    access_token: String,
+    refresh_token: String,
+    token_type: String,
+    expiry_date: Number,
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  displayName: String,
+  avatarUrl: String,
+});
+
+const User = mongoose.model('User', mongoSchema);
+
+module.exports = User;
