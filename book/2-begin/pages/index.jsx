b1fd5fbc2854718f7e064897a0752fd1a27a14ca
Nicholas Josten <josten.n01@mymail.sxu.edu>
2022-09-11 22:54:17 -0500

    Chapter two should now be full commited after this.

--- book/2-begin/pages/index.jsx
+++ book/2-begin/pages/index.jsx
@@ -1,13 +1,35 @@
+import PropTypes from 'prop-types';
+
 import Head from 'next/head';
 
-const Index = () => (
+const propTypes = {
+  user: PropTypes.shape({
+    displayName: PropTypes.string,
+    email: PropTypes.string.isRequired,
+  }),
+};
+
+const defaultProps = {
+  user: null,
+};
+
+const Index = ({ user }) => (
   <div style={{ padding: '10px 45px' }}>
     <Head>
-      <title>Index page</title>
-      <meta name="description" content="This is SEO description of Index page" />
+      <title>Dashboard</title>
+      <meta name="description" content="This is a description of the Index page" />
     </Head>
     <p>Content on Index page</p>
+    <p>
+      Email:&nbsp;
+      {user.email}
+    </p>
   </div>
 );
+
+Index.getInitialProps = async (ctx) => ({ user: ctx.query.user });
+
+Index.propTypes = propTypes;
+Index.defaultProps = defaultProps;
 
 export default Index;
