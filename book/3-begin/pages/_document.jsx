151ab13d88970fb2484cdace5dae3aada9e7f3c9
Jose Paz <me@pazjose.com>
2022-09-18 14:15:47 -0500

    Chapter 3 work

--- book/3-begin/pages/_document.jsx
+++ book/3-begin/pages/_document.jsx
@@ -76,6 +76,11 @@ class MyDocument extends Document {
           />
           <link rel="stylesheet" href="https://storage.googleapis.com/builderbook/vs.min.css" />
 
+          <link
+            rel="stylesheet"
+            href="https://storage.googleapis.com/builderbook/nprogress.min.css"
+          />
+
           <style>
             {`
               a {
---
+++ book/3-begin/pages/login.jsx
@@ -0,0 +1,28 @@
+import Head from 'next/head';
+import Button from '@mui/material/Button';
+
+import withAuth from '../lib/withAuth';
+import { styleLoginButton } from '../components/SharedStyles';
+
+const Login = () => (
+  <div style={{ textAlign: 'center', margin: '0 20px' }}>
+    <Head>
+      <title>Log in to Builder Book</title>
+      <meta name="description" content="Login page for builderbook.org" />
+    </Head>
+    <br />
+    <p style={{ margin: '45px auto', fontSize: '44px', fontWeight: '400' }}>Log in</p>
+    <p>You’ll be logged in for 14 days unless you log out manually.</p>
+    <br />
+    <Button variant="contained" style={styleLoginButton} href="/auth/google">
+      <img
+        src="https://storage.googleapis.com/builderbook/G.svg"
+        alt="Log in with Google"
+        style={{ marginRight: '10px' }}
+      />
+      Log in with Google
+    </Button>
+  </div>
+);
+
+export default withAuth(Login, { logoutRequired: true });
