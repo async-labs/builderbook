7503d3f3162c6b35bd269f0650b9a11f340b0849
Nicholas Josten <josten.n01@mymail.sxu.edu>
2022-09-20 18:59:02 -0500

    Chapter three is completed.

---
+++ book/3-begin/server/google.js
@@ -0,0 +1,84 @@
+const passport = require('passport');
+const Strategy = require('passport-google-oauth').OAuth2Strategy;
+const User = require('./models/User');
+
+function setupGoogle({ server, ROOT_URL }) {
+  const verify = async (accessToken, refreshToken, profile, verified) => {
+    let email;
+    let avatarUrl;
+
+    if (profile.emails) {
+      email = profile.emails[0].value;
+    }
+
+    if (profile.photos && profile.photos.length > 0) {
+      avatarUrl = profile.photos[0].value.replace('sz=50', 'sz=128');
+    }
+
+    try {
+      const user = await User.signInOrSignUp({
+        googleId: profile.id,
+        email,
+        googleToken: { accessToken, refreshToken },
+        displayName: profile.displayName,
+        avatarUrl,
+      });
+      verified(null, user);
+    } catch (err) {
+      verified(err);
+      console.log(err); // eslint-disable-line
+    }
+  };
+  passport.use(
+    new Strategy(
+      {
+        clientID: process.env.GOOGLE_CLIENTID,
+        clientSecret: process.env.GOOGLE_CLIENTSECRET,
+        callbackURL: `${ROOT_URL}/oauth2callback`,
+      },
+      verify,
+    ),
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id, User.publicFields(), (err, user) => {
+      done(err, user);
+    });
+  });
+
+  server.use(passport.initialize());
+  server.use(passport.session());
+
+  server.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email'],
+      prompt: 'select_account',
+    }),
+  );
+
+  server.get(
+    '/oauth2callback',
+    passport.authenticate('google', {
+      failureRedirect: '/login',
+    }),
+    (_, res) => {
+      res.redirect('/');
+    },
+  );
+
+  server.get('/logout', (req, res, next) => {
+    req.logout((err) => {
+      if (err) {
+        next(err);
+      }
+      res.redirect('/login');
+    });
+  });
+}
+
+module.exports = setupGoogle;
---
+++ book/3-begin/server/utils/slugify.js
@@ -0,0 +1,27 @@
+const _ = require('lodash');
+
+const slugify = (text) => _.kebabCase(text);
+
+async function createUniqueSlug(Model, slug, count) {
+  const user = await Model.findOne({ slug: `${slug}-${count}` }, 'id');
+
+  if (!user) {
+    return `${slug}-${count}`;
+  }
+
+  return createUniqueSlug(Model, slug, count + 1);
+}
+
+async function generateSlug(Model, name, filter = {}) {
+  const origSlug = slugify(name);
+
+  const user = await Model.findOne({ slug: origSlug, ...filter }, 'id');
+
+  if (!user) {
+    return origSlug;
+  }
+
+  return createUniqueSlug(Model, origSlug, 1);
+}
+
+module.exports = generateSlug;
